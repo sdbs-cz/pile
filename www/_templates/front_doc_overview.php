33eeeec9e460110109c7252e0e12f55dd4bab8ed
Tomáš Mládek <tomas.mladek@urc-systems.cz>
2017-03-08 10:10:23 +0100

    don't show empty attrs in overview

--- www/_templates/front_doc_overview.php
+++ www/_templates/front_doc_overview.php
@@ -1,9 +1,15 @@
 <div class="text document">
     <h1><?= $doc["Title"] ?></h1>
+    
+    <?php if (!empty($doc["Author"])): ?>
     <h2><?= $doc["Author"] ?></h2>
+    <?php endif; ?>
+    
     <?php if (!empty($doc["Published"])): ?>
     <h3>Published: <?= $doc["Published"] ?></h3>
     <?php endif; ?>
+    
+    <?php if (!empty($doc["tags"])): ?>
     <h3 class="doc-taglist">Tags:
         <?
         foreach($doc["tags"] as $tag){
@@ -11,6 +17,11 @@
         }
         ?>
     </h3>
+    <?php endif; ?>
+    
+    <?php if (!empty($doc["Description"])): ?>
     <p class="doc-description"><span class="doc-description-intro">Description: </span><?= $doc["Description"] ?></p>
+    <?php endif; ?>
+    
     <div class="doc-link"><span class="doc-link-intro">Access file at: </span><a href="<?= $doc["URL"] ?>"><?= urldecode($doc["URL"]) ?></a></div>
 </div>
