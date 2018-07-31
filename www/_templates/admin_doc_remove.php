d5fbe1c3410220e2f094ae5d02465272a6387c15
Tomáš Mládek <tmladek@inventati.org>
2018-07-31 15:44:32 +0200

    make remove msg more accurate

--- www/_templates/admin_doc_remove.php
+++ www/_templates/admin_doc_remove.php
@@ -1,4 +1,4 @@
 <div class="text">
     <p>Confirm deletion of <strong>"<?= $doc["Title"] ?>"</strong>:</p>
-    <a href="admin.php?action=remove&confirm=yes&item=<?= $doc["ID"] ?>&ret=<?= $_SERVER['HTTP_REFERER']; ?>" class="button">Remove from database (and drive)</a>
+    <a href="admin.php?action=remove&confirm=yes&item=<?= $doc["ID"] ?>&ret=<?= $_SERVER['HTTP_REFERER']; ?>" class="button">Remove from database</a>
 </div>
