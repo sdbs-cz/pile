108f0c383777d06130188604267e061839488433
Tomáš Mládek <t@mldk.cz>
2020-01-16 22:30:36 +0100

    add goatcounter analytics

--- www/_templates/front_wrap.php
+++ www/_templates/front_wrap.php
@@ -70,5 +70,17 @@
         </form>
     <?php endif; ?>
 </div>
+
+<script>
+	(function() {
+		window.counter = 'https://sdbs_pile.goatcounter.com/count'
+
+		var script = document.createElement('script');
+		script.async = 1;
+		script.src = '//gc.zgo.at/count.js';
+		var ins = document.getElementsByTagName('script')[0];
+		ins.parentNode.insertBefore(script, ins)
+	})();
+</script>
 </body>
 </html>
