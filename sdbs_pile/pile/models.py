3af8a7e1a612d90cb13fbffc32b62a2664b11360
Tomáš Mládek <t@mldk.cz>
2020-06-10 10:13:28 +0200

    do not require related document

--- sdbs_pile/pile/models.py
+++ sdbs_pile/pile/models.py
@@ -57,7 +57,7 @@ class Document(SoftDeletableModel):
                               max_length=3, choices=DocumentStatus.choices, default=DocumentStatus.STANDARD)
     tags = models.ManyToManyField(Tag, related_name="documents", blank=True)
     uploaded = models.DateTimeField(auto_now_add=True, null=True)
-    related = models.ManyToManyField('self', related_name='related')
+    related = models.ManyToManyField('self', related_name='related', blank=True)
 
     objects = DocumentManager()
 
